3a6f554ede5f6f6f10cd695af8bcda04f5c39b2c
yangjingsheng <826821901@qq.com>
2017-09-28 18:35:56 +0800

    add cuci and simple ui logic

---
+++ app/src/main/java/com/ifading/namemachine/NameActivity.java
@@ -0,0 +1,44 @@
+package com.ifading.namemachine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by yangjingsheng on 17/9/28.
+ */
+
+public class NameActivity extends AppCompatActivity{
+    @BindView(R.id.name_activity_tv_last_name)
+    protected TextView mTvLastName;
+    @BindView(R.id.name_activity_tv_mid_name)
+    protected TextView mTvMidName;
+    @BindView(R.id.name_activity_tv_first_name)
+    protected TextView mTvFirstName;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        String lastName = intent.getStringExtra(MainActivity.LAST_NAME);
+        String midName = intent.getStringExtra(MainActivity.MID_NAME);
+        if (midName == null){
+            mTvMidName.setVisibility(View.INVISIBLE);
+        }else{
+            mTvMidName.setText(midName);
+        }
+        mTvLastName.setText(lastName);
+
+    }
+}
